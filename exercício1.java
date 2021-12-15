03832949201f63d417d1b355e5c7505571ff41ff
ThaisSantiago <95662321+ThaisSantiago@users.noreply.github.com>
2021-12-15 01:25:17 -0300

    Add files via upload

---
+++ exercício1.java
@@ -0,0 +1,21 @@
+package decisao;
+
+import java.util.Scanner;
+
+public class exercício1 {
+
+	public static void main(String[] args)
+	{
+		
+		Scanner leia = new Scanner(System.in);
+		int n8, n24, n72, nMaior;
+		
+		
+		System.out.println("Entre com o maior número:");
+		nMaior=leia.nextInt();
+		
+		
+		
+	}
+
+}
